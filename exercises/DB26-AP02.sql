bec74a6cff2512396f0bca21d9674f148d1224c4
Bernardo Camilo <151168745+BernardoCarmo2007@users.noreply.github.com>
2026-09-25 08:54:19 +0000

    update

---
+++ exercises/DB26-AP02.sql
@@ -0,0 +1,12 @@
+/*
+  Lesson 02 - Expressions, NULL, conversion, and formatting
+  Student: a22509596
+  Class: D01EINF02
+  Date: 25/09/2026
+*/
+
+USE ULHT_DB26;
+GO
+
+--A1. Formal name
+Select E
